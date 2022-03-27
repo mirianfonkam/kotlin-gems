6a3df7abc86b169f32382e9a58b5d2f930c7921d
mirianfonkam <28691174+mirianfonkam@users.noreply.github.com>
2022-03-27 01:57:47 +0000

    Update: API documentation

--- src/main/kotlin/Main.kt
+++
@@ -1,8 +0,0 @@
-import java.util.*
-
-/**
- * @suppress
- */
-fun main() {
-    println("Hello, world!".replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() })
-}
